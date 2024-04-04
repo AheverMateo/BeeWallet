3cf00e5f31ed64e6b20d0c3f9275f09cb94b36f4
Enok <business.libay@gmail.com>
2024-04-04 16:05:30 -0500

    some

--- Front/src/app/dashboard.tsx
+++ Front/src/app/dashboard.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom"
+import { DashboardSidebar } from "./ui/dahsboard-sidebar"
 
 export default function Dashboard() {
     return (
-        <div className="size-full bg-red">
-            <Outlet />
+        <div className="size-full">
+            <DashboardSidebar />
+            <div>
+                <Outlet />
+            </div>
         </div>
     )
 }
--- Front/src/app/ui/footer.tsx
+++ Front/src/app/ui/footer.tsx
@@ -1,4 +1,7 @@
+import {ruc2} from '@/assets/scripts/ruc.js'
+
 export default function Footer() {
+    ruc2();
     return (
         <div>
             <h1>Footer</h1>
