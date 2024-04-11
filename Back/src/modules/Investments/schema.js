a2986740b0b55647a533ab7d2c2c222be962f5ef
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-10 23:44:17 -0300

    feat: update investment scheme

--- Back/src/modules/Investments/schema.js
+++ Back/src/modules/Investments/schema.js
@@ -3,10 +3,9 @@ import { Schema, Types, model } from "mongoose";
 const investmentSchema = new Schema(
    {
       days: { type: Number, required: true },
-      amount: { type: Types.Decimal128, required: true },
-      currency: { type: String, required: true, default: "ARS" },
+      amount: { type: Number, required: true },
       tna: { type: Number, required: true },
-      earnedInterests: { type: Types.Decimal128 },
+      earnedInterests: { type: Number },
       finishDate: { type: Date },
       walletId: { type: Types.ObjectId, ref: "Wallet", required: true },
    },
