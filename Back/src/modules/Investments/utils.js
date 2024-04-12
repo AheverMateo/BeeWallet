b81121997367bd7fddf09ce21ea837b97d9c7b46
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-12 17:46:24 -0300

    refractor: update function calculate earned interests

--- Back/src/modules/Investments/utils.js
+++ Back/src/modules/Investments/utils.js
@@ -1,13 +1,13 @@
-export const TNA = 0.7;
+export const TNA = 0.6;
 
 export function calculateEarnedInterests(amount, days) {
    //earnedInterests = amount * (tna * days / 365)
-   return amount * ((TNA * days) / 365);
+   return parseFloat((amount * ((TNA * days) / 365)).toFixed(2));
 }
 
 export function getFinishDate(days) {
    let finishDate = new Date();
    finishDate.setDate(finishDate.getDate() + days);
-   
+
    return finishDate;
 }
