8408ee09640b04de2c645cf14d8f35197ff3beb7
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-15 18:52:30 -0300

    [back] feat: update investment schema

--- Back/src/modules/Investments/schema.js
+++ Back/src/modules/Investments/schema.js
@@ -5,7 +5,7 @@ const investmentSchema = new Schema(
       days: { type: Number, required: true },
       amount: { type: Number, required: true },
       tna: { type: Number, required: true },
-      earnedInterests: { type: Number },
+      earnedInterests: { type: Types.Decimal128 },
       finishDate: { type: Date },
       walletId: { type: Types.ObjectId, ref: "Wallet", required: true },
    },
