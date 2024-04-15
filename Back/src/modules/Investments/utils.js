87f066e26337877ea348148a02794a642a21551f
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-15 18:52:59 -0300

    [back] feat: update utils

--- Back/src/modules/Investments/utils.js
+++ Back/src/modules/Investments/utils.js
@@ -1,8 +1,10 @@
+import BigNumber from "bignumber.js";
+
 export const TNA = 0.6;
 
 export function calculateEarnedInterests(amount, days) {
    //earnedInterests = amount * (tna * days / 365)
-   return parseFloat((amount * ((TNA * days) / 365)).toFixed(2));
+   return new BigNumber(amount * ((TNA * days) / 365)).toFixed(2).toString();
 }
 
 export function getFinishDate(days) {
