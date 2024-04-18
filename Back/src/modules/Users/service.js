6c1feaf4ac24aa9c36e56af2e5eb1cc365fd32b4
Valentín <thenasky@gmail.com>
2024-04-18 19:09:34 -0300

    auth admin crud/schema fix

--- Back/src/modules/Users/service.js
+++ Back/src/modules/Users/service.js
@@ -1,133 +1,134 @@
 import MailingService from "../Mailing/service.js";
 
 class UsersService {
-   static generateVerificationCode() {
-      return Math.floor(100000 + Math.random() * 900000); // Generates a 6-digit random number
-   }
+  static generateVerificationCode() {
+    return Math.floor(100000 + Math.random() * 900000); // Generates a 6-digit random number
+  }
 
-   static storeVerificationCodeInSession(req, code) {
-      req.session.verificationCode = code;
-      req.session.codeExpiry = Date.now() + 600000; // Code valid for 10 minutes
-   }
+  static storeVerificationCodeInSession(req, code) {
+    req.session.verificationCode = code;
+    req.session.codeExpiry = Date.now() + 600000; // Code valid for 10 minutes
+  }
 
-   static checkVerificationCodeInSession(req, providedCode) {
-      if (
-         req.session.verificationCode === providedCode &&
-         req.session.codeExpiry > Date.now()
-      ) {
-         return true; // Code is correct and not expired
-      }
-      return false; // Code is incorrect or expired
-   }
+  static checkVerificationCodeInSession(req, providedCode) {
+    if (req.session.verificationCode === providedCode && req.session.codeExpiry > Date.now()) {
+      return true; // Code is correct and not expired
+    }
+    return false; // Code is incorrect or expired
+  }
 
-   static async initiateVerificationProcess(req, email) {
-      const code = UsersService.generateVerificationCode();
-      UsersService.storeVerificationCodeInSession(req, code);
-      await MailingService.sendEmailCodeVerification(email, code);
-   }
-   static async getUsers() {
-      try {
-         const users = await UsersModel.find({});
-         return resSuccess(200, "Displaying all Users", { users });
-      } catch (error) {
-         logger.error(`${error.stack}`);
-         return resFail(500, "Internal Server Error");
+  static async initiateVerificationProcess(req, email) {
+    const code = UserService.generateVerificationCode();
+    UserService.storeVerificationCodeInSession(req, code);
+    await MailingService.sendEmailCodeVerification(email, code);
+  }
+  static async getUsers() {
+    try {
+      const users = await UsersModel.find({});
+      return [200, "Displaying all Users", { users }];
+    } catch (error) {
+      logger.error(`${error.stack}`);
+      return [500, "Internal Server Error"];
+    }
+  }
+  static async getUser(id) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return [400, "Invalid user ID"];
       }
-   }
-   static async getUser(id) {
-      try {
-         if (!mongoose.Types.ObjectId.isValid(id)) {
-            return resFail(400, "Invalid user ID");
-         }
-         const user = await UsersModel.findOne({ _id: id }).populate({
-            path: "wishlist.product",
-            model: "products",
-         });
-         if (!user) {
-            return resFail(400, "User Not Found");
-         }
-         return resSuccess(200, "Displaying User: " + id, { user });
-      } catch (error) {
-         logger.error(`${error.stack}`);
-         return resFail(500, "Internal Server Error");
+      const user = await UsersModel.findOne({ _id: id }).populate({
+        path: "wishlist.product",
+        model: "products",
+      });
+      if (!user) {
+        return [400, "User Not Found"];
       }
-   }
-   static async deleteUser(id) {
-      try {
-         if (!mongoose.Types.ObjectId.isValid(id)) {
-            return resFail(400, "Invalid user ID");
-         }
-         const user = await UsersModel.findOne({ _id: id });
-         if (!user) {
-            return resFail(400, "User Not Found");
-         }
-         await UsersModel.deleteOne({ _id: id });
-         return resSuccess(200, "User " + id + " Deleted");
-      } catch (error) {
-         logger.error(`${error.stack}`);
-         return resFail(500, "Internal Server Error");
+      return [200, "Displaying User: " + id, { user }];
+    } catch (error) {
+      logger.error(`${error.stack}`);
+      return [500, "Internal Server Error"];
+    }
+  }
+  static async deleteUser(id) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return [400, "Invalid user ID"];
       }
-   }
-   static async updateUser(id, updatedProperties) {
-      try {
-         if (!mongoose.Types.ObjectId.isValid(id)) {
-            return resFail(400, "Invalid user ID");
-         }
-         const user = await UsersModel.findOne({ _id: id });
-         if (!user) {
-            return resFail(400, "User Not Found");
-         }
-         if ("password" in updatedProperties) {
-            return resFail(400, "Password cannot be updated");
-         }
-         const propertiesToUpdate = Object.keys(updatedProperties).filter(
-            (key) => key !== "_id" && key !== "password" && user[key] !== undefined
-         );
-         propertiesToUpdate.forEach((key) => {
-            user[key] = updatedProperties[key];
-         });
-         await user.save({ new: true });
+      const user = await UsersModel.findOne({ _id: id });
+      if (!user) {
+        return [400, "User Not Found"];
+      }
+      await UsersModel.deleteOne({ _id: id });
+      return [200, "User " + id + " Deleted"];
+    } catch (error) {
+      logger.error(`${error.stack}`);
+      return [500, "Internal Server Error"];
+    }
+  }
+  static async updateUser(id, updatedProperties) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return [400, "Invalid user ID"];
+      }
+      const user = await UsersModel.findOne({ _id: id });
+      if (!user) {
+        return [400, "User Not Found"];
+      }
+      if ("password" in updatedProperties) {
+        return [400, "Password cannot be updated"];
+      }
+      const propertiesToUpdate = Object.keys(updatedProperties).filter(
+        (key) => key !== "_id" && key !== "password" && user[key] !== undefined
+      );
+      propertiesToUpdate.forEach((key) => {
+        user[key] = updatedProperties[key];
+      });
+      await user.save({ new: true });
 
-         return resSuccess(200, "User " + id + " Updated Successfully", {
-            updatedUser: user,
-         });
-      } catch (error) {
-         logger.error(`${error.stack}`);
-         return resFail(500, "Internal Server Error");
+      return [
+        200,
+        "User " + id + " Updated Successfully",
+        {
+          updatedUser: user,
+        },
+      ];
+    } catch (error) {
+      logger.error(`${error.stack}`);
+      return [500, "Internal Server Error"];
+    }
+  }
+  static async blockUser(id) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return [400, "Invalid user ID"];
       }
-   }
-   static async blockUser(id) {
-      try {
-         if (!mongoose.Types.ObjectId.isValid(id)) {
-            return resFail(400, "Invalid user ID");
-         }
-         const user = await UsersModel.findOne({ _id: id });
-         if (!user) {
-            return resFail(400, "User Not Found");
-         }
-         await UsersModel.findByIdAndUpdate(id, { isBlocked: true }, { new: true });
-         return resSuccess(200, "User " + id + " Blocked");
-      } catch (error) {
-         logger.error(`${error.stack}`);
-         return resFail(500, "Internal Server Error");
+      const user = await UsersModel.findOne({ _id: id });
+      if (!user) {
+        return [400, "User Not Found"];
       }
-   }
-   static async unblockUser(id) {
-      try {
-         if (!mongoose.Types.ObjectId.isValid(id)) {
-            return resFail(400, "Invalid user ID");
-         }
-         const user = await UsersModel.findOne({ _id: id });
-         if (!user) {
-            return resFail(400, "User Not Found");
-         }
-         await UsersModel.findByIdAndUpdate(id, { isBlocked: false }, { new: true });
-         return resSuccess(200, "User " + id + " unBlocked");
-      } catch (error) {
-         logger.error(`${error.stack}`);
-         return resFail(500, "Internal Server Error");
+      await UsersModel.findByIdAndUpdate(id, { isBlocked: true }, { new: true });
+      return [200, "User " + id + " Blocked"];
+    } catch (error) {
+      logger.error(`${error.stack}`);
+      return [500, "Internal Server Error"];
+    }
+  }
+  static async unblockUser(id) {
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return [400, "Invalid user ID"];
       }
-   }
+      const user = await UsersModel.findOne({ _id: id });
+      if (!user) {
+        return [400, "User Not Found"];
+      }
+      await UsersModel.findByIdAndUpdate(id, { isBlocked: false }, { new: true });
+      return [200, "User " + id + " unBlocked"];
+    } catch (error) {
+      logger.error(`${error.stack}`);
+      return [500, "Internal Server Error"];
+    }
+  }
 }
 
 export default UsersService;
