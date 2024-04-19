331bc0ae1d9545b3eeb9a1ab5ecdc0e0b547f7fd
Mateo <brandonahumada804@gmail.com>
2024-04-19 12:28:31 -0300

    user dashboard

--- Front/src/app/ui/user.tsx
+++ Front/src/app/ui/user.tsx
@@ -1,74 +1,116 @@
-
-
+import SideBar from "../dashboard/SideBar"
+import imgUser from "../../assets/icons/unsplash_ZXfUUM_LR0k.png"
+import download from "../../assets/icons/download.svg"
+import discard from "../../assets/icons/discard.svg"
+import logo from "../../assets/icons/Logo.svg"
 const user = () => {
 
     return (
         <div className="flex">
-
-
-        <div className="w-[70%]">
-                    <section className="mt-12 w-[80%] ml-20">
-                            <div>
-                                <h1 className="text-xl font-bold text-white">Personal</h1>
-                                <hr className="w-full h-px border-1 my-4 border-yellow-600" />
-                                <div className=" flex w-full rounded-lg px-6 py-12 mt-5 bg-zinc-900">
-                                    <img className="h-14 w-14 mr-10" src="https://cdn-icons-png.flaticon.com/512/9131/9131529.png" alt="" />
-                                    <h4 className="mt-4 text-white">Nombre y Apellido:</h4>
-                                    <span></span>
+            <div className="w-[10%] ml-10 mt-24 fixed">
+                <SideBar/>
+            </div>
+            <div className="flex justify-end w-[100%]">
+                    <section className="mt-12 w-[65%] mr-[10%]">
+                                <h1 className="text-2xl font-bold text-white">Personal</h1>
+                                <div className=" flex w-full rounded-xl px-6 py-4 mt-8 bg-zinc-900">
+                                    <img className="ml-2" src={imgUser} alt="" />
+                                    <div className=" flex flex-col ml-10 justify-center">
+                                    <h4 className=" text-gray-400 text-sm">Nombre y Apellido</h4>
+                                    <p className="text-gray-300 text-xl font-bold">Jane Doe</p>
+                                    </div>
                                 </div>
-                                <div className=" flex w-full rounded-lg px-6 py-6 mt-5 bg-zinc-900">
-                                    <ul className="flex flex-col">
-                                        <li className="text-white">Telefono:</li>
-                                        <li className="text-white">Email:</li>
-                                        <li className="text-white">Fecha de nacimiento:</li>
-                                        <li className="text-white">Direccion:</li>
-                                        <li className="text-white">Pais:</li>
-                                    </ul>
+
+                                <div className=" flex flex-col w-full rounded-xl px-8 py-6 mt-10 bg-zinc-900">
+                                    <div>
+                                    <h4 className="text-gray-400 text-sm mb-2">Telefono</h4>
+                                    <p className="text-gray-300 text-xl mb-2">+54******0552</p>
+                                    <hr className="border-gray-400" />
+                                    </div>
+                                    <div>
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">Correo electrónico</h4>
+                                    <p className="text-gray-300 text-xl mb-2">janedoe.2024@outlook.com</p>
+                                    <hr className="border-gray-400" />
+                                    </div>
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">Fecha de Nacimiento</h4>
+                                    <p className="text-gray-300 text-xl mb-2">00/00/0000</p>
                                 </div>
-                                <div className=" flex w-full rounded-lg px-6 py-10 mt-5 bg-zinc-900">
-                                    <ul className="flex flex-col">
-                                        <li className="text-white">Datos Fiscales:</li>
-                                        <li className="text-white">Cuit/Cuil:</li>
-                                        <li className="text-white">Ocupacion:</li>
-                                    </ul>
+
+                                <div className=" flex flex-col w-full rounded-xl px-8 py-6 mt-10 bg-zinc-900">
+                                    <h4 className="text-gray-400 text-sm mb-2">Dirección (calle y número)</h4>
+                                    <p className="text-gray-300 text-xl mb-2">Avenida Córdoba 1234.</p>
+                                    <hr className="border-gray-400" />
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">Código Postal</h4>
+                                    <p className="text-gray-300 text-xl mb-2">1234</p>
+                                    <hr className="border-gray-400" />
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">Ciudad y País</h4>
+                                    <p className="text-gray-300 text-xl mb-2">Buenos Aires, Argentina.</p>
+                                </div>
+                            <div className=" mt-20">
+                                <h1 className="text-2xl font-bold text-white mb-8">Privacidad</h1>
+                                <div className=" flex flex-col w-full rounded-xl px-6 py-4 mt-5 bg-zinc-900">
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">Contraseña</h4>
+                                    <p className="text-gray-300 text-xl mb-2">Cal123456#</p>
+                                    <hr className="border-gray-400" />
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">PIN</h4>
+                                    <p className="text-gray-300 text-xl mb-2">0000</p>
+                                </div>
+
+                                <div className=" flex flex-col w-full rounded-xl px-6 py-4 mt-5 bg-zinc-900">
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">Tipo de empleo </h4>
+                                    <p className="text-gray-300 text-xl mb-2">Autónomo - Full time</p>
+                                    <hr className="border-gray-400" />
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">Experiencia</h4>
+                                    <p className="text-gray-300 text-xl mb-2">1-3 años</p>
+                                </div>
+                            </div>
+
+                            <div className=" mt-20">
+                                <h1 className="text-2xl font-bold text-white mb-8">Documentos</h1>
+                                <div className="  flex flex-col w-full rounded-xl px-6  mt-5 bg-zinc-900">
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">Número de documento</h4>
+                                    <p className="text-gray-300 text-xl mb-2">00.000.000</p>
+                                    <hr className="border-gray-400" />
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">CUIL/CUIT </h4>
+                                    <p className="text-gray-300 text-xl mb-2">00-00000000-0</p>
+                                </div>
+
+                                <div className="flex w-full rounded-xl px-6  mt-5 bg-zinc-900">
+                                    <img className="w-6 h-6 mt-3" src={download} alt="" />
+                                    <div className=" flex flex-col ml-6 justify-center">
+                                    <h4 className=" text-gray-400 text-sm">Documento</h4>
+                                    <p className="text-gray-300 text-xl font-bold">PDF, JPEG, PNG</p>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div className=" mt-28">
-                                <h1 className="text-xl font-bold text-white">Privacidad</h1>
-                                <hr className="w-full h-px border-1 my-4 border-yellow-600" />
-
-                                <div className=" flex flex-col w-full rounded-lg px-6 py-12 mt-5 bg-zinc-900">
-                                    <p className="text-white">Contrseña:</p>
-                                    <p className="text-white">PIN:</p>
-                                </div>
-
-                                <h4 className=" mt-8 text-xl text-white">Documentos</h4>
-                                <div className=" flex w-full rounded-lg px-6 py-12 mt-3 bg-zinc-900">
-                                    <p className="text-white">ID o identificacion:</p>
+                            <div className=" mt-20">
+                                <h1 className="text-2xl font-bold text-white mb-8">Configuracion</h1>
+                                <div className="  flex flex-col w-full rounded-xl  px-6 py-10  mt-5 bg-zinc-900">
+                                <h4 className="text-gray-400 text-sm mt-4 mb-2">AAAAAAAAAAA</h4>
+                                    <p className="text-gray-300 text-xl mb-2">aAAAAAAAAAAAAAA</p>
+                                    <hr className="border-gray-400" />
+                                    <h4 className="text-gray-400 text-sm mt-4 mb-2">AAAAAAAAAAAAA</h4>
+                                    <p className="text-gray-300 text-xl mb-2">AAAAAAAAAAAAAAAAAAA</p>
+                                    <hr />
                                 </div>
                             </div>
 
-                            <div className=" mt-28">
-                                <h1 className="text-xl font-bold text-white">Configuracion</h1>
-                                <hr className="w-full h-px border-1 my-4 border-yellow-600" />
-                                <div className=" flex w-full rounded-lg px-6 py-12 mt-5 bg-zinc-900">
-                                    <h4></h4>
-                                    <p></p>
-                                    <button>
-                                        buscar
-                                    </button>
-                                </div>
-
-                                <div className=" flex w-full rounded-lg px-6 py-12 mt-5 bg-zinc-900">
-                                <h4>Info de la cuenta</h4>
-                                    <p></p>
-                                    <button>
-                                        buscar
-                                    </button>
+                            <div className="flex flex-col justify-center items-center mt-20">
+                                <button className="border-yellow-400 border rounded-full px-10 py-2 text-yellow-300">Cerrar Sesion</button>
+                                <div className="mt-10 flex">
+                                <img className="mr-2" src={discard} alt="" />
+                                <p className="text-gray-300 cursor-pointer">Eliminar Cuenta</p>
                                 </div>
                             </div>
+                            <hr className="border-gray-400 mt-10" />
+                            <footer className=" flex justify-between py-6 mt-5">
+                                <img src={logo} alt="" />
+                                <p className="text-gray-300 text-end">© Beewallet 2024. Todos los derechos reservados.<br/>
+                                Sitio web diseñado y desarrollado por Beewalle.</p>
+                            </footer>
                     </section>
+
         </div>
     </div>
     )
