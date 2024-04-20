9dd99a1419011013cf589687a41cd96aef6cc783
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-20 00:16:16 -0300

    add initial config to passport with google

---
+++ Back/src/config/passport.google.js
@@ -0,0 +1,24 @@
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import passport from "passport";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+      scope: ["profile", "email"],
+    },
+    function (accessToken, refreshToken, profile, callback) {
+      // todo: agregar código para buscar el usuario en la BD
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
