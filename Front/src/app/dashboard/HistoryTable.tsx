7d52cce0b3d7cb309849ec19c5d08165133fb31d
Ivan Aguirre <viac321@gmail.com>
2024-04-20 16:31:07 -0600

    Arreglos en el dashboard inicial

--- Front/src/app/dashboard/HistoryTable.tsx
+++ Front/src/app/dashboard/HistoryTable.tsx
@@ -4,8 +4,9 @@ const Table = () => {
   return (
     
 
-<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table className="w-full text-sm text-left rtl:text-right bg-[#232323] text-gray-500 dark:text-gray-400">
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg overflow-y-auto max-h-80">
+    <table className="w-full text-sm text-left rtl:text-right
+     bg-[#232323] text-gray-500 dark:text-gray-400 overflow-hidden">
         <thead className="text-xs uppercase">
             <tr>
                 <th scope="col" className="px-6 py-3">
@@ -77,6 +78,74 @@ const Table = () => {
                     -50.000
                 </td>
             </tr>
+            <tr className="bg-[#232323] border-b  dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Jane Doe
+                </th>
+                <td className="px-6 py-4">
+                    Transferencia
+                </td>
+                <td className="px-6 py-4">
+                    Enviada
+                </td>
+                <td className="px-6 py-4">
+                    11 Mar 2023
+                </td>
+                <td className="px-6 py-4 text-right">
+                    -50.000
+                </td>
+            </tr>
+            <tr className="bg-[#232323] border-b  dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Jane Doe
+                </th>
+                <td className="px-6 py-4">
+                    Transferencia
+                </td>
+                <td className="px-6 py-4">
+                    Enviada
+                </td>
+                <td className="px-6 py-4">
+                    11 Mar 2023
+                </td>
+                <td className="px-6 py-4 text-right">
+                    -50.000
+                </td>
+            </tr>
+            <tr className="bg-[#232323] border-b  dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Jane Doe
+                </th>
+                <td className="px-6 py-4">
+                    Transferencia
+                </td>
+                <td className="px-6 py-4">
+                    Enviada
+                </td>
+                <td className="px-6 py-4">
+                    11 Mar 2023
+                </td>
+                <td className="px-6 py-4 text-right">
+                    -50.000
+                </td>
+            </tr>
+            <tr className="bg-[#232323] border-b  dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Jane Doe
+                </th>
+                <td className="px-6 py-4">
+                    Transferencia
+                </td>
+                <td className="px-6 py-4">
+                    Enviada
+                </td>
+                <td className="px-6 py-4">
+                    11 Mar 2023
+                </td>
+                <td className="px-6 py-4 text-right">
+                    -50.000
+                </td>
+            </tr>
             <tr className="bg-[#232323]  dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     Jane Doe
@@ -94,6 +163,7 @@ const Table = () => {
                     -50.000
                 </td>
             </tr>
+            
         </tbody>
     </table>
 </div>
---
+++ Front/src/app/dashboard/Transferencia/ej.tsx
@@ -0,0 +1,62 @@
+<div
+      className='size-full min-h-screen min-w-fit bg-[#0E0E0E] text-[#FCFFFF] md:flex md:justify-center p-3 '
+    >
+      <Outlet />
+      <main className="flex justify-center gap-3">
+        <section className="hidden md:block h-100%">
+          <SideBar />
+        </section>
+        <div>
+          <section className="p-3 relative flex justify-between">
+            <div className=" md:hidden">
+              <SideBarHeader1 />
+            </div>
+            <br />
+            <HeadR />
+          </section>
+          <section className=" max-md:flex max-md:justify-center max-md:flex-col">
+            <div
+              className="bg-transparent gap-1 
+            min-sm:w-[70rem] min-sm:h-[26.5625rem] flex flex-col items-center justify-center"
+            >
+              <div className="md:flex md:gap-5 md:justify-center">
+                <section className="flex flex-col p-5 gap-5 ">
+                  <div className="bg-[#232323] rounded-[1.15625rem] p-1
+                  max-sm:w-[27.125rem] max-sm:h-[15rem]">
+                    <div className="flex flex-col gap-2 max-sm:pl-6 max-smw-[28rem]">
+                      <div className="flex justify-between max-sm:pl-1 max-sm:pt-2 max-sm:pr-1">
+                      <h2 className="max-sm:mb-2 md:text-lg">Cuenta Beelancer</h2>
+                      <div className="relative right-0">
+                        <Link to={"/dashboard/CVU"}
+                          className="border-[#FCFFFF] border-[1px] text-[0.875rem]
+                       w-[7.125rem] h-[2.3125rem] bg-[#323131] rounded-[1.15625rem]"
+                        >
+                          {" "}
+                          Tu CVU{" "}
+                        </Link>
+                      </div>
+                      </div>
+                    
+                      <AccountData1 />
+                    </div>
+                    
+                  </div>
+                  <div className=" ">
+                      <AcData2 />
+                    </div>
+                </section>
+                <section className=" ">
+                  <AccountData3 />
+                </section>
+              </div>
+            </div>
+            <div className=" md:flex md:justify-center">
+              <Movements />
+            </div>
+          </section>
+          <section className="md:hidden">
+            <SideBarFooter />
+          </section>
+        </div>
+      </main>
+    </div>
