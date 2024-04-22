2a15add55afe52858d5a51434f1590043a6f6f0d
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-22 17:54:36 -0300

    feat: add google auth route

---
+++ Back/src/modules/Users/GoogleAuth/router.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import passport from "passport";
+
+const router = Router();
+
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: "Successfully Loged In",
+      user: req.user,
+    });
+  } else {
+    res.status(403).json({ error: true, message: "Not Authorized" });
+  }
+});
+
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    error: true,
+    message: "Log in failure",
+  });
+});
+
+router.get("/", passport.authenticate("google", ["profile", "email"]));
+
+router.get(
+  "/callback",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/api/auth/google/login/failed",
+  })
+);
+
+router.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.session.destroy();
+    res.redirect(process.env.CLIENT_URL);
+  });
+});
+
+export default router;
