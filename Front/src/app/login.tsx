6290f8369ac73d8e403d5afb16559107e609408e
NicolasIppoliti <contacto.nicolasippoliti@gmail.com>
2024-04-25 17:25:59 -0300

    frontend login connection and header & footer fix

--- Front/src/app/login.tsx
+++ Front/src/app/login.tsx
@@ -23,7 +23,7 @@ const Login: React.FC<LoginProps> = function Login() {
 
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const response = await fetch("http://3.84.44.177/api/auth/login", {
+    const response = await fetch("http://localhost:3000/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
